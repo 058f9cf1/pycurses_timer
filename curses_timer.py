8178a125294e5c0fe774e0057b0e47f319e4a192
058f9cf1 <058f9cf1@tutanota.com>
2023-09-21 13:10:55 +0100

    Time is now truncated rather than rounded up

--- curses_timer.py
+++ curses_timer.py
@@ -7,10 +7,12 @@ import templates
 
 
 def error_check(t):
+	#Check if t is an integer
 	try:
 		t = int(t)
 	except ValueError:
 		return 1
+	#Check if t is positive
 	if(t<1):
 		return 1
 	return 0
@@ -28,29 +30,47 @@ def run_timer(screen, t):
 	curses.curs_set(0)
 	screen.nodelay(1)
 	curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+	#Loop whilst the time has not elapsed
 	while(key_pressed != ord('q') and t > 0):
+		#Clear the screen
 		screen.erase()
+		#Get screen size
 		rows, cols = screen.getmaxyx()
+		
+		#TODO: Check if screen is too small
+		#What is the minimum screen size?
+		
+		#Subtract a second
+		t -= 1
+		#Convert seconds to minutes and seconds
 		m, s = divmod(t, 60)
+		#Add leading zeroes to the time if only one digit
 		digits = str(m).zfill(2) + str(s).zfill(2)
+		#Display digits on the screen
 		draw_digit(screen, rows//2 - 5, cols//2 - 28, digits[0])
 		draw_digit(screen, rows//2 - 5, cols//2 - 14, digits[1])
 		draw_digit(screen, rows//2 - 5, cols//2 + 4, digits[2])
 		draw_digit(screen, rows//2 - 5, cols//2 + 18, digits[3])
+		#Display the colon between minutes and seconds
 		screen.addstr(rows//2+1,cols//2,"  ",curses.color_pair(1))
 		screen.addstr(rows//2-1,cols//2,"  ",curses.color_pair(1))
-		t -= 1
+		#Show the new digits
 		screen.refresh()
+		#Check if the user quits
 		key_pressed = screen.getch()
+		#Wait 1 second
 		time.sleep(1)
 
 
 if  __name__ == "__main__":
+	#Try getting timer length from arguments
 	try:
 		t = sys.argv[1]
+	#Error if no argument given
 	except IndexError:
 		print("ERROR: No argument given")
 	else:
+		#Check if the argument is in the correct format
 		if(not error_check(t)):
 			curses.wrapper(run_timer, int(t))
 		else:
