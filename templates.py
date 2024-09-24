da9f6b4d3eb27d70d2f0163733a70e91ab5c2ff6
Blahaj <182660640+blahajzone@users.noreply.github.com>
2024-09-24 22:49:25 +0100

    Digits can be resized

--- templates.py
+++ templates.py
@@ -4,15 +4,15 @@ def templates(n):
 	r = [0,0,0,0,0,0,0,0,1,1,1,1]
 	g = [1,1,1,1,0,0,0,0,1,1,1,1]
 
-	template_list =[[f,f,g,g,g,g,g,g,f,f],	#0
-			[r,r,r,r,r,r,r,r,r,r],	#1
-			[f,f,r,r,f,f,l,l,f,f],	#2
-			[f,f,r,r,f,f,r,r,f,f],	#3
-			[g,g,g,g,f,f,r,r,r,r],	#4
-			[f,f,l,l,f,f,r,r,f,f],	#5
-			[f,f,l,l,f,f,g,g,f,f],	#6
-			[f,f,r,r,r,r,r,r,r,r],	#7
-			[f,f,g,g,f,f,g,g,f,f],	#8
-			[f,f,g,g,f,f,r,r,f,f]]	#9
+	template_list =[[f,g,g,g,f],	#0
+					[r,r,r,r,r],	#1
+					[f,r,f,l,f],	#2
+					[f,r,f,r,f],	#3
+					[g,g,f,r,r],	#4
+					[f,l,f,r,f],	#5
+					[f,l,f,g,f],	#6
+					[f,r,r,r,r],	#7
+					[f,g,f,g,f],	#8
+					[f,g,f,r,f]]	#9
 
 	return template_list[n]
